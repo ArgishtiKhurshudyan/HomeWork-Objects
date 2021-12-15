bfcc4be8ef97e7b9388ebac7376adb85b56765cf
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2021-12-16 01:04:51 +0400

    objects

---
+++ object.js
@@ -0,0 +1,8 @@
+//use destructuring
+let person = { name: "Sarah", country: "Nigeria", job: "Developer" };
+const { name, country, job  } = person;
+
+console.log(name);
+console.log(country); 
+console.log(job);
+
